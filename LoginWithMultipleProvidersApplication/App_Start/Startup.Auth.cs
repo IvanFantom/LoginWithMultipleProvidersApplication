748c72569581a96660804019076466fd7dc3c9f9
Sergey Bedulin <sergey.bedulin@itechart-group.com>
2015-07-06 12:30:13 +0300

    Initial commit

---
+++ LoginWithMultipleProvidersApplication/App_Start/Startup.Auth.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Helpers;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Owin.Security.Providers.LinkedIn;
+
+namespace LoginWithMultipleProvidersApplication
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.UseGoogleAuthentication(
+                clientId: "394343389150-pjmd9ce2g22e37pqq7qa2q96911jfsh4.apps.googleusercontent.com",
+                clientSecret: "TGPdiGlibRlSVEC2dJMX64fK");
+
+            app.UseFacebookAuthentication(
+                appId: "1461968390770111",
+                appSecret: "e0272de1a390958bbd8cee50e1dcdfb8");
+
+            app.UseLinkedInAuthentication(
+                clientId: "774gbhxjezyr25",
+                clientSecret: "nfkVDomTl0XytDHi");
+        }
+    }
+}
